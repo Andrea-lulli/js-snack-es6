690607a185713e450811aebe43f7351b20649e5d
Andrea-Lulli <andrea.lulli89@gmail.com>
2022-11-08 16:25:40 +0100

    es

--- esercizio 3/main.js
+++ esercizio 3/main.js
@@ -5,12 +5,40 @@ Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa
  con peso minore utilizzando destructuring e template literal
 */
 
+let stampa = document.getElementById("stampa")
+
 const bici = [
 {
-    nome: "bianchi";
-    peso: 7;
+    nome: "bianchi",
+    peso: 5,
+},
+{
+    nome: "de rosa",
+    peso: 4,
+},
+{
+    nome: "ducati",
+    peso: 8,
+},
+{
+    nome: "carrera",
+    peso: 10,
+},
+{
+    nome: "rossi",
+    peso: 9,
 }
 
 ]
 
-console.log(bici);
+console.log(Math.min ( ...bici).peso);
+
+const elenco = bici.filter( ( element ) =>{
+    if(Math.min(element)) {
+return true 
+    }
+    stampa.innerHTML += element
+})
+
+console.log(elenco);
+
---
+++ esercizio 4/main.js
@@ -0,0 +1,61 @@
+/*
+Snack4
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà 
+diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica 
+proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà: Punti fatti 
+e falli subiti. Infine usando la destrutturazione creiamo un nuovo array 
+i cui elementi contengono solo nomi e falli subiti e 
+stampiamo tutto in console.
+*/
+
+
+
+const squadre = [
+    {
+        nome: "roma",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "lazio",
+        puntiFatti: 0,
+        falliSubiti: 0,
+
+    },
+    {
+        nome: "inter",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "juve",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "milan",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    }
+    
+    ]
+
+
+
+squadre.forEach((element)=>{
+    element.puntiFatti = Math.floor(Math.random()* 30)+1
+    element.falliSubiti = Math.floor(Math.random()* 50)+1
+})
+
+console.log(squadre);
+
+let nuovoArray = squadre.filter (({nome, falliSubiti}) =>{
+   console.log({nome, falliSubiti}); 
+   return true 
+    
+    
+
+})   
+
+
