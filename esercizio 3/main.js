dc082ce826416046347cb45e64d61dec73aa5e65
Andrea-Lulli <andrea.lulli89@gmail.com>
2022-11-08 15:00:44 +0100

    stuttura

---
+++ esercizio 3/main.js
@@ -0,0 +1,16 @@
+/*   
+Snack 3
+Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa
+ con le seguenti proprietà: nome e peso. Stampare a schermo la bici 
+ con peso minore utilizzando destructuring e template literal
+*/
+
+const bici = [
+{
+    nome: "bianchi";
+    peso: 7;
+}
+
+]
+
+console.log(bici);
